1c14fc979e51613d3b979beb264fa1f7cbaae76c
Arjen Norbart <arjennorbart@gmail.com>
2025-10-06 14:41:05 +0200

    Make day 2 part 1, Red-Nosed Reports

---
+++ AOC.2024/Components/Pages/RedNosedReports.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AOC._2024.Components.Pages;
+
+public partial class RedNosedReports : ComponentBase
+{
+    private int safeReports = 0;
+
+    private void FindSafeReports()
+    {
+        var file = File.ReadLines("day2.txt");
+        foreach (var report in file)
+        {
+            // List levels
+            var array = report.Split(" ");
+            var levels = Array.ConvertAll(array, int.Parse).ToList();
+            
+            // Filter out all increasing/decreasing levels that contain no duplicates
+            if (!levels.SequenceEqual(levels.Order().Distinct()) && !levels.SequenceEqual(levels.OrderDescending().Distinct()))
+                continue;
+            
+            // Adjacent level difference cannot be more than 3
+            var valid = true;
+            for (int i = 1; i < levels.Count; i++)
+            {
+                if (Math.Abs(levels[i - 1] - levels[i]) > 3)
+                    valid = false;
+            }
+
+            if (valid)
+            {
+                safeReports++;
+            }
+        }
+    }
+}
